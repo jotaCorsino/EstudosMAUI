34657421a35c13528e19afae268d5d0139469e95
Joao Corsino <oi.corsino@gmail.com>
2024-10-04 10:18:44 -0300

    Exercicio de Binding e listar dados na tela

---
+++ ListaDeItens/ListaDeItens/Models/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeItens.Models
+{
+    public class Item
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public Item()
+        {
+
+        }
+
+        public Item(string nome, string descricao)
+        {
+            Nome = nome;
+            Descricao = descricao;
+        }
+    }
+}
